191b80dccb3352336d2d2e05db6d2898df4cb880
michaelchevallier <michael.chevallier@gmail.com>
2015-11-28 23:50:38 +0000

    itoa upgraded

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -1,11 +1,9 @@
 #include "libft.h"
 #include <stdlib.h>
-#include <stdio.h>
-
 
 static int		ft_isneg(int n)
 {
-	int neg;
+	int		neg;
 
 	neg = 0;
 	if (n < 0)
@@ -13,33 +11,48 @@ static int		ft_isneg(int n)
 	return (neg);
 }
 
+static void		*ft_strrev(char *str)
+{
+	int		i;
+	int		length;
+	char	tmp;
+
+	if (str)
+	{
+		i = 0;
+		length = ft_strlen(str);
+		while (i < length / 2)
+		{
+			tmp = str[i];
+			str[i] = str[length - i - 1];
+			str[length - i - 1] = tmp;
+			i++;
+		}
+	}
+	return (str);
+}
+
 static char		*calc(int n, int i, char *chain)
 {
 	int		isneg;
-	char	*result;
-	int		j;
 
 	isneg = ft_isneg(n);
-	result = (char *)ft_memalloc(12);
-	j = 0;
 	if (isneg == 1)
-	{
 		n = n * -1;
-		i++;
-	}
 	while (n > 0)
 	{
 		chain[i] = ((n % 10 + 48));
 		n = n / 10;
 		i++;
 	}
-	while (i >= 0)
+	if (isneg == 1)
 	{
-		result[j] = chain [i];
-		i--;
-		j++;
+		chain[i] = '-';
+		i++;
 	}
-	return(&result[i + 2]);
+	chain[i] = '\0';
+	ft_strrev(chain);
+	return(chain);
 }
 
 char			*ft_itoa(int n)
@@ -50,21 +63,17 @@ char			*ft_itoa(int n)
 
 	i = 0;
 	isneg = ft_isneg(n);
-	chain = (char *)ft_memalloc(12);
+	chain = (char *)ft_memalloc(14);
 	if (n == -2147483648)
-		{
-			chain = "-2147483648";
-			return(chain);
-		}
+	{
+		chain = "-2147483648";
+		return (chain);
+	}
 	if (n == 0)
 	{
 		chain[i] = 48;
-		return(&chain[i]);
+		return (&chain[i]);
 	}
 	chain = calc(n, i, chain);
-	while (chain[i] == '0')
-		i++;
-	if (isneg == 1)
-		chain[i - 1] = '-';
-	return((isneg == 1) ? &chain[i - 1] : chain);
+	return (chain);
 }
