9a803d4b940039b86361798c50d21e8df0c9573b
michaelchevallier <michael.chevallier@gmail.com>
2015-11-30 12:31:43 +0100

    ft_putnbr

--- libft/ft_putnbr.c
+++ libft/ft_putnbr.c
@@ -2,5 +2,21 @@
 
 void		ft_putnbr(int n)
 {
-	ft_putstr(ft_itoa(n));
+	int		temp;
+
+	temp = 0;
+	if (n == -2147483648)
+		ft_putstr("-2147483648");
+	else if (n < 0)
+	{
+		ft_putchar('-');
+		n = -n;
+	}
+	if (n > 0)
+	{
+		temp = n / 10;
+		ft_putnbr(temp);
+		ft_putchar(n % 10 + 48);
+		n = temp;
+	}
 }
