0fab859d8dc8f44e6e30a2b2a9939f2743fbf95c
Michael CHEVALLIER <mchevall@e2r5p16.42.fr>
2016-01-05 18:24:34 +0100

    update

---
+++ ft_strsplit.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mchevall <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/03 10:46:16 by mchevall          #+#    #+#             */
+/*   Updated: 2015/12/08 15:21:02 by mchevall         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t		ft_size(char const *s, char c, size_t pos)
+{
+	size_t		i;
+	size_t		j;
+
+	if (!s || !c)
+		return (0);
+	i = 0;
+	j = 0;
+	while (s[pos + i] == c && s)
+		i++;
+	while (s[pos + i + j] != c && s)
+		j++;
+	return (j);
+}
+
+static size_t		ft_countword(char const *s, char c)
+{
+	size_t		i;
+	size_t		nbword;
+
+	i = 0;
+	nbword = 0;
+	while (s[i])
+	{
+		while (s[i] == c && s[i])
+			i++;
+		while (s[i] != c && s[i])
+			i++;
+		nbword++;
+	}
+	if (s[i - 1] == c)
+		nbword--;
+	return (nbword);
+}
+
+static size_t		ft_startword(char const *s, char c, size_t pos)
+{
+	while (s[pos] == c && s)
+		pos++;
+	return (pos--);
+}
+
+char				**ft_strsplit(char const *s, char c)
+{
+	size_t		pos;
+	size_t		i;
+	size_t		j;
+	char		**str;
+
+	pos = 0;
+	i = 0;
+	j = 0;
+	if (!s || !c)
+		return (NULL);
+	str = (char **)ft_memalloc(sizeof(char *) * (ft_countword(s, c) + 1));
+	while (i < ft_countword(s, c) && str)
+	{
+		pos = ft_startword(s, c, pos);
+		str[i] = (char *)ft_memalloc(sizeof(char) * ((ft_size(s, c, pos) + 1)));
+		while (j < ft_size(s, c, pos))
+		{
+			str[i][j] = s[pos + j];
+			j++;
+		}
+		pos = pos + j;
+		j = 0;
+		i++;
+	}
+	return (str);
+}
